577ffcdb6473e781f37a64aebf05e1daa4d79a5f
azrulwm <azrulazmi.wm@gmail.com>
2025-02-25 23:31:20 +0800

    feat: Added prettier config

--- src/components/layout/navbar/Navbar.tsx
+++ src/components/layout/navbar/Navbar.tsx
@@ -1,30 +1,27 @@
-"use client";
+'use client';
 
-import Link from "next/link";
-import { useState } from "react";
-import Image from "next/image";
-import mainLogo from "@public/main-logo.svg";
-import { ROUTES } from "@/config/route";
+import Link from 'next/link';
+import { useState } from 'react';
+import Image from 'next/image';
+import mainLogo from '@public/main-logo.svg';
+import { ROUTES } from '@/config/route';
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const navLinks = [
-    { href: ROUTES.ABOUT, label: "About" },
-    { href: ROUTES.RECRUITMENT, label: "Recruitment" },
-    { href: ROUTES.HR_CONSULTANCY, label: "HR Consultancy" },
-    { href: ROUTES.CONTACT, label: "Contact Us" },
+    { href: ROUTES.ABOUT, label: 'About' },
+    { href: ROUTES.RECRUITMENT, label: 'Recruitment' },
+    { href: ROUTES.HR_CONSULTANCY, label: 'HR Consultancy' },
+    { href: ROUTES.CONTACT, label: 'Contact Us' },
   ];
 
   return (
-    <nav className="border-b border-gray-200 bg-white">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <nav className="bg-primary">
+      <div className="container">
         <div className="flex h-16 justify-between">
           <div className="flex items-center">
-            <Link
-              href="/"
-              className="text-xl font-bold text-gray-900 hover:text-gray-700"
-            >
+            <Link href={ROUTES.HOME}>
               <Image
                 src={mainLogo}
                 alt="Brand Logo"
@@ -42,7 +39,7 @@ const Navbar = () => {
               <Link
                 key={link.href}
                 href={link.href}
-                className="text-gray-600 transition-colors duration-200 hover:text-gray-900"
+                className="font-bold text-secondary"
               >
                 {link.label}
               </Link>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,8 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#45523E",
+        secondary: "#B4B548",
       },
     },
   },
